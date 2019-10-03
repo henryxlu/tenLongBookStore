cbbd4d761ee4bb3cfc5c7b3e000cfdd2fa0c2d20
henryLu <henryxlu220@gmail.com>
2019-10-03 14:58:59 +0800

    Initial Commit

---
+++ tenLongBookstore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tenLongBookstore
+//
+//  Created by Henry on 2019/10/3.
+//  Copyright © 2019 Henry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
